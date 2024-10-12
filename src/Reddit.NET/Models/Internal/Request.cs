0f06717278fdd80caf30d368fc698f478b8717cc
David Jones <david.jones@swanisland.net>
2024-10-11 20:29:11 -0700

    Hotfix: integrating change mentioned on git issues board. Don't know why a PR hasn't been submitted yet but this will be temporary solution

--- src/Reddit.NET/Models/Internal/Request.cs
+++ src/Reddit.NET/Models/Internal/Request.cs
@@ -172,6 +172,7 @@ namespace Reddit.Models.Internal
                 while ((res == null || !res.IsSuccessful)
                         && (RefreshToken != null || DeviceId != null)
                         && (res.StatusCode == HttpStatusCode.Unauthorized  // This is returned if the access token needs to be refreshed or wasn't provided.  --Kris
+                            || res.StatusCode == HttpStatusCode.Forbidden // Since 2024-07 it seems that Reddit returns Forbidden instead of Unauthorized when no token is present. --janssen-io (Stan)
                             || res.StatusCode == HttpStatusCode.InternalServerError  // On rare occasion, a valid request will return a status code of 500, particularly if under heavy load.  --Kris
                             || res.StatusCode == 0)  // On rare occasion, a valid request will return a status code of 0, particularly if under heavy load.  --Kris
                         && retry > 0)
